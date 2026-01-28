e87345a8b1e088f9386c2e5747baca1d7abb1e5f
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-01-28 10:15:24 -0700

    added

--- PortalProject/script.js
+++ PortalProject/script.js
@@ -1,4 +1,14 @@
+
+/* Date and year*/
+
 let currentDate = new Date().toLocaleDateString();
 let date = document.querySelector("#date");
 
 date.textContent = currentDate;
+
+let currentYear = new Date().getFullYear();
+let year = document.createElement("span");
+year.textContent = currentYear;
+document.querySelector("footer").appendChild(year);
+
+/*   asdfasdf */
