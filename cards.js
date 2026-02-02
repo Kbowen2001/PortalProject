db14694d8f17e36acc1ea37046ba3283be65113a
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-02 14:05:20 -0700

    added

--- cards.js
+++ cards.js
@@ -6,17 +6,27 @@
 
 let cardsList = document.querySelector(".cards");
 let cards = [];
-
+let card1 = { title: "Mario Project", url: "MarioProject/index.html" };
+cards.push(card1);
+cards.map((item) => {
+  cardsList.innerHTML += `
+    <div class="card">
+      <div class="card-inner">
+        <div class="card-front">
+          <h2>${item.title}</h2>
+        </div>
+        <div class="card-back">
+          <a href="${item.url}">Project link</a>
+        </div>
+      </div>
+    </div>
+  `;
+});
 
 // look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
 
 
 // Look up how to push an object to an array
 
-write the following code to start creating your map:
+//write the following code to start creating your map:
 
-cards.map((item) => {
-
-})
-
-${card.title}
