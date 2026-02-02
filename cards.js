b6913e416c2352e16118e43e2cb6fe83acebd1c6
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-02 13:43:22 -0700

    added comments

--- cards.js
+++ cards.js
@@ -1,8 +1,16 @@
+//The first one is an array of titles and links
+//The next is using a loop to render your html.
+//Lets give you a starting point, look at this code below and some of it you will find familiar.
+
+
+
 let cardsList = document.querySelector(".cards");
 let cards = [];
 
 
 // look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
+
+
 // Look up how to push an object to an array
 
 write the following code to start creating your map:
