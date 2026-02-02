a1a6b21e55b83364ba46fb0fb23bb79acd5de954
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-02 13:42:16 -0700

    added js

---
+++ cards.js
@@ -0,0 +1,14 @@
+let cardsList = document.querySelector(".cards");
+let cards = [];
+
+
+// look up what an object in JavaScript is, when you find the syntax for an object, include a title and a url link
+// Look up how to push an object to an array
+
+write the following code to start creating your map:
+
+cards.map((item) => {
+
+})
+
+${card.title}
